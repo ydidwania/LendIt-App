f8555539775af6e032557135890066af0676419b
ydidwania <didwaniayashvardhan+github@gmail.com>
2017-08-28 09:43:36 +0530

    code cleanup

--- app/src/main/java/com/iitb/ferozepurwale/lendit_app/MainActivity.java
+++ app/src/main/java/com/iitb/ferozepurwale/lendit_app/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -18,20 +17,21 @@ public class MainActivity extends AppCompatActivity {
     LoginButton loginButton;
     TextView textView;
     CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginButton = (LoginButton)findViewById(R.id.fb_login_id);
-        final TextView skipButton = (TextView)findViewById(R.id.skip_button);
+        loginButton = (LoginButton) findViewById(R.id.fb_login_id);
+        final TextView skipButton = (TextView) findViewById(R.id.skip_button);
         //textView = (TextView)findViewById(R.id.status);
         callbackManager = CallbackManager.Factory.create();
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent mainToHome=new Intent(MainActivity.this , HomeActivity.class);
-                mainToHome.putExtra("logged_in",true);
-                mainToHome.putExtra("user_id",loginResult.getAccessToken().getUserId());
+                Intent mainToHome = new Intent(MainActivity.this, HomeActivity.class);
+                mainToHome.putExtra("logged_in", true);
+                mainToHome.putExtra("user_id", loginResult.getAccessToken().getUserId());
                 startActivity(mainToHome);
                 //textView.setText(loginResult.getAccessToken().getUserId());
             }
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mainToHome=new Intent(MainActivity.this , HomeActivity.class);
-                mainToHome.putExtra("logged_in",false);
+                Intent mainToHome = new Intent(MainActivity.this, HomeActivity.class);
+                mainToHome.putExtra("logged_in", false);
                 startActivity(mainToHome);
             }
         });
